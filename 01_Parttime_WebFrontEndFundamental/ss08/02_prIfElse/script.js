f67a1515bc347cee443b6f3cefd67959672e7f13
ToanPham <tientoan2096@gmail.com>
2023-01-10 23:37:17 +0900

    Session 8: Practice2: If else

---
+++ 01_Parttime_WebFrontEndFundamental/ss08/02_prIfElse/script.js
@@ -0,0 +1,8 @@
+function ifElse() {
+    let value = prompt('What is the "official" name of JavaScript?', '');
+    if (value == 'ECMAScript') {
+        alert('Right!');
+    } else {
+        alert("You don't know? ECMAScript!");
+    }
+}
